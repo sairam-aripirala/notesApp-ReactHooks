c6e150d0738499fe8c007f681cbb15f23baf8547
sairam-aripirala <aripiralasairam@gmail.com>
2023-07-22 09:26:38 +0000

    notesapp

---
+++ src/components/NoteItem/index.js
@@ -0,0 +1,15 @@
+import {EachNoteCon, NoteHeading, NotePara} from './styledComponents'
+
+const NoteItem = props => {
+  const {noteDetails} = props
+  const {title, note} = noteDetails
+
+  return (
+    <EachNoteCon>
+      <NoteHeading>{title}</NoteHeading>
+      <NotePara>{note}</NotePara>
+    </EachNoteCon>
+  )
+}
+
+export default NoteItem
